5a0ef58a1afc0d59b95fec759919e8ca7bda5db4
intel1590 <radhikaintelera@gmail.com>
2025-10-31 20:34:19 +0530

    Fixed aiemail config issue

--- config/aiemail.php
+++ config/aiemail.php
@@ -2,10 +2,13 @@
 
 return [
     'client_class' => env('AIEMAIL_CLIENT_CLASS', \OmDiaries\AIEmailAssistant\Support\Adapters\OpenAIAdapter::class),
+
     'default_options' => [
         'temperature' => 0.7,
         'max_tokens' => 800,
     ],
 
     'openai_api_key' => env('OPENAI_API_KEY', ''),
+
+    'track_emails' => true,
 ];
