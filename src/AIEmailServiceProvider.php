c1144961e8e4b81a67a0de86dffdba37e7004ebb
intel1590 <radhikaintelera@gmail.com>
2025-10-31 21:49:43 +0530

    Added Adapter

--- src/AIEmailServiceProvider.php
+++ src/AIEmailServiceProvider.php
@@ -16,14 +16,19 @@ class AIEmailServiceProvider extends ServiceProvider
 
     public function register()
 {
-    $this->mergeConfigFrom(__DIR__.'/../config/aiemail.php', 'aiemail');
+    $this->mergeConfigFrom(__DIR__ . '/../config/aiemail.php', 'aiemail');
 
     $this->app->singleton(AIEmailGenerator::class, function ($app) {
         $clientClass = config('aiemail.client_class');
         $apiKey = config('aiemail.openai_key');
         $client = new $clientClass($apiKey);
+
         return new AIEmailGenerator($client, config('aiemail'));
     });
+
+    // 👇 add this line to create an alias for app('ai-email')
+    $this->app->alias(AIEmailGenerator::class, 'ai-email');
 }
+
 
 }
