810d60c4501cd9b5f57184bd956085ac00262b99
intel1590 <radhikaintelera@gmail.com>
2025-11-02 13:03:16 +0530

    Resolved merge conflicts for beta release

--- src/Contracts/AIClientInterface.php
+++ src/Contracts/AIClientInterface.php
@@ -2,6 +2,7 @@
 
 namespace OmDiaries\AIEmailAssistant\Contracts;
 
+
 interface AIClientInterface
 {
     public function generateEmail(string $prompt, string $tone = 'friendly', string $output = 'plain'): string;
